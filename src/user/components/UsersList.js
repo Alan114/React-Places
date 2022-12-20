94af9d510ff93c8fbcd61d342e5f637b31b257a8
Alan Pecenkovic <alanquad@yahoo.com>
2022-12-20 11:40:12 +0100

    sending a GET request to get users

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -3,7 +3,7 @@ import "./UsersList.css";
 import UserItem from "./UserItem";
 import Card from "../../shared/components/UIElements/Card";
 
-const UsersList = props => {
+const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
@@ -16,13 +16,13 @@ const UsersList = props => {
 
   return (
     <ul className="users-list">
-      {props.items.map(user => (
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.places}
+          placeCount={user.places.length}
         />
       ))}
     </ul>
